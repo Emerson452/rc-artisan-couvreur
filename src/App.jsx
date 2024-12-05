0a3f37baa97565adf27747891e4af20f2cc96c81
Emerson452 <emerson@popoyoko.fr>
2024-12-05 20:02:34 +0100

    🎨

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import { useState } from "react";
 import Header from "./components/header";
 import "./App.css";
+import toiture from "./assets/toiture.jpeg";
 
 function App() {
   return (
     <>
       <Header></Header>
+      <img id="toiture" src={toiture} alt="toiture" />
+      <div className="highlighted">
+        <ul>
+          <li>Qualité et disponibilité</li>
+          <li>Devis gratuit et tarifs honnêtes</li>
+          <li>Service reconnu et primé</li>
+          <li>Intervention dans les meilleurs délais</li>
+        </ul>
+      </div>
     </>
   );
 }
